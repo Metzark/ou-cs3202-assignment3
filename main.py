3b92617f15fe55c2df9d7826362a5ed831bd929b
jonasmetzger1 <jonasmetzger@uflip.com>
2023-02-15 16:37:06 -0600

    product function

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ def sumArray(arr):
     for i in range(0, len(arr)):
         sum += arr[i]
     return sum
+
+def productArray(arr):
+    product = 1;
+    for i in range(0, len(arr)):
+        product *= arr[i]
+    return product
+
